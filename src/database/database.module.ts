f85a73ce35c2c24ae8aa16b089e73aeb29b010d1
Darvand <davinchis95@gmail.com>
2024-02-24 11:38:45 -0500

    Updating mongo connection string

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Logger, Module } from "@nestjs/common";
 import { ConfigModule, ConfigService, ConfigType } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import databaseConfig from "./database.config";
@@ -6,10 +6,13 @@ import databaseConfig from "./database.config";
 @Module({
     imports: [
         MongooseModule.forRootAsync({
-            useFactory: async (config: ConfigType<typeof databaseConfig>) => ({
-                uri: `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/retryWrites=true&w=majority&appName=hogar-dev`,
-                dbName: config.dbName,
-            }),
+            useFactory: async (config: ConfigType<typeof databaseConfig>) => {
+                Logger.log(`Connecting to database: ${`mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/?retryWrites=true&w=majority&appName=hogar-dev`}`)
+                return {
+                    uri: `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/retryWrites=true&w=majority&appName=hogar-dev`,
+                    dbName: config.dbName,
+                }
+            },
             inject: [databaseConfig.KEY],
             imports: [ConfigModule]
         })
